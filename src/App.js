4affc569f707f1cf239235a55b3410b1bd4cf835
Eddy <you@example.com>
2021-04-08 18:05:46 +0300

    lesson 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,98 @@
-import React from "react";
+import React, { Component, useState, useEffect } from "react";
 
-function App() {
+let interval;
+
+// class Comp extends Component {
+//   componentDidMount() {
+//     interval = setInterval(() => {
+//       console.log("fetching new data about airplanes");
+//     }, 2000);
+//     console.log("component did mount child");
+//   }
+
+//   componentWillUnmount() {
+//     console.log("componentWillUnmount");
+
+//     clearInterval(interval);
+//   }
+
+//   render() {
+//     console.log("child render");
+//     return <h1> Child </h1>;
+//   }
+// }
+
+// class App extends Component {
+//   state = {
+//     counter: 0,
+//   };
+
+//   //Ця штука потрібна для того,щоб реагувати на якісь внутрішні і звонішні зміни,після рірендеру
+
+//   componentDidMount() {
+//     console.log("component did mount");
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log("component did update");
+//   }
+
+//   incCounter = () => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+
+//   render() {
+//     console.log("parent render");
+//     return (
+//       <>
+//         <h1 onClick={this.incCounter}>LifeCycle {this.state.counter}</h1>
+//         {!!(this.state.counter % 2) && <Comp />}
+//       </>
+//     );
+//   }
+// }
+// export default App;
+
+const Comp = () => {
+  useEffect(() => {
+    interval = setInterval(() => {
+      console.log("fetching new data about airplanes");
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+  return <h1>child</h1>;
+};
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  //use Effect - штука де ми записуємо побічні дії
+
+  useEffect(() => {
+    console.log("did mount parent");
+  }, []);
+
+  useEffect(() => {
+    return () => console.log("did mount parent");
+  }, []);
+
+  useEffect(() => {
+    console.log("did update parent");
+
+    return () => {
+      console.log("parent unmount");
+    };
+  }, [counter]);
   return (
-    <div className="App">
-      <h1>Ed</h1>
-    </div>
+    <>
+      <h1 onClick={incCounter}>LifeCycle {counter}</h1>
+      {!!(counter % 2) && <Comp />}
+    </>
   );
-}
+};
 
 export default App;
