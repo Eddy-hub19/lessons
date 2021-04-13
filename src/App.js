5250794f36ade9e0d856737be10b9588236cc01f
Eddy <you@example.com>
2021-04-13 14:49:48 +0300

    Lesson 3

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import React, { useState, useEffect } from "react";
 
-const url = "https://jsonplaceholder.typicode.com/todos";
+const url = "https://jsonplaceholder.typicode.com/todos/";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [todos, setTodos] = useState([]);
+  const [counter, setCounter] = useState(1);
+  const [todo, setTodo] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const incCounter = () => {
     setCounter(counter + 1);
   };
 
   const fetchTodos = async () => {
-    const response = await fetch(url);
+    setIsLoading(true);
+    const response = await fetch(`${url}${counter}`); // 'https://jsonplaceholder.typicode.com/todos/2'
     const data = await response.json();
 
-    setTodos(data);
+    // setTimeout(() => {
+    setTodo(data);
+    setIsLoading(false);
+    // }, 1000);
   };
-
   useEffect(() => {
     fetchTodos();
-  }, []);
+    return () => {
+      setTodo(null);
+    };
+  }, [counter]);
 
   return (
     <>
       <h1 onClick={incCounter}>HELLO REACT {counter}</h1>
-      {!!todos.length && (
-        <div>
+
+      {!todo && isLoading && <h1> LOADING DATA... </h1>}
+
+      {!!todo && (
+        <>
           <hr />
-          <h3>{todos[0].title} - {todos[0].completed.toString()}</h3>
+          <h3>
+            {todo.title} - {todo.completed.toString()} {todo.id}
+          </h3>
           <hr />
-        </div>
+        </>
       )}
     </>
   );
