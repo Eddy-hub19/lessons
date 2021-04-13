275c722d7c3075bb5b681fe16193c179bb519bdc
Eddy <you@example.com>
2021-04-13 15:02:07 +0300

    new branch Home-works

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import React, { useState, useEffect } from "react";
+import ListsPost from "./Components/Component/Component";
 
 const url = "https://jsonplaceholder.typicode.com/todos/";
 
 const App = () => {
-  const [counter, setCounter] = useState(1);
-  const [todo, setTodo] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const incCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const fetchTodos = async () => {
-    setIsLoading(true);
-    const response = await fetch(`${url}${counter}`); // 'https://jsonplaceholder.typicode.com/todos/2'
-    const data = await response.json();
-
-    // setTimeout(() => {
-    setTodo(data);
-    setIsLoading(false);
-    // }, 1000);
-  };
-  useEffect(() => {
-    fetchTodos();
-    return () => {
-      setTodo(null);
-    };
-  }, [counter]);
-
   return (
-    <>
-      <h1 onClick={incCounter}>HELLO REACT {counter}</h1>
-
-      {!todo && isLoading && <h1> LOADING DATA... </h1>}
-
-      {!!todo && (
-        <>
-          <hr />
-          <h3>
-            {todo.title} - {todo.completed.toString()} {todo.id}
-          </h3>
-          <hr />
-        </>
-      )}
-    </>
+    <div>
+      <ListsPost />
+      
+    </div>
   );
 };
-
 export default App;
---
+++ src/Components/Component/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ListsPost extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <button>1</button>
+      </div>
+    );
+  }
+}
+
+export default ListsPost;
