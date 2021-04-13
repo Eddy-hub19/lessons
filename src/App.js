97d00e84e1fa7751f7166d7fe8fb7ae83aa52039
Eddy <you@example.com>
2021-04-13 11:57:03 +0300

    new branch Study

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 
-const url = "https://jsonplaceholder.typicode.com/todos";
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [todos, setTodos] = useState([]);
-
-  const incCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const fetchTodos = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    setTodos(data);
-  };
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  return (
+function App() {
+  return(
     <>
-      <h1 onClick={incCounter}>HELLO REACT {counter}</h1>
-      {!!todos.length && (
-        <div>
-          <hr />
-          <h3>{todos[0].title} - {todos[0].completed.toString()}</h3>
-          <hr />
-        </div>
-      )}
+    <h1> События </h1>
+    <section>
+      <h2> Button </h2>
+      <button> Push </button>
+    </section>
     </>
-  );
-};
-
-export default App;
+  )
+}
