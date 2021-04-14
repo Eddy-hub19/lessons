55f4da40f2d362a899cf2f91265e4378fad419ab
Eddy <you@example.com>
2021-04-14 19:19:31 +0300

    hw-3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+
+/* <>
+          {selectedTab === POSTS && <PostList posts={list} />}
+          {selectedTab === COMMENTS && <CommentList comments={list} />}
+          {selectedTab === ALBUMS && <PhotoList photos={list} />}
+          {selectedTab === PHOTOS && <AlbumList albums={list} />}
+          {selectedTab === TODOS && <TodoList todos={list} />}
+          {selectedTab === USERS && <UserList users={list} />}
+        </> */
 
 // урл
 // https://jsonplaceholder.typicode.com/
@@ -21,27 +30,155 @@ import "./App.css";
 
 const Tabs = ({ tabs, selectedTab }) => {
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       {tabs.map((tab) => (
-        <button style={{background: selectedTab === tab.title ? "green" : "lightgray",}}onClick={tab.clickHandler}>{tab.ttile}</button>))}
+        <button
+          key={tab.title}
+          style={{
+            flex: 1,
+            height: "50px",
+            background: selectedTab === tab.title ? "green" : "lightgrey",
+          }}
+          onClick={tab.clickHandler}
+        >
+          {tab.title}
+        </button>
+      ))}
     </div>
   );
+};
+const PostList = ({ list }) => {
+  return (
+    <>
+      {list.map((post) => (
+        <div key={post.id}>
+          <h3> {post.title} </h3>
+          <p> {post.body} </p>
+        </div>
+      ))}
+    </>
+  );
+};
+const CommentList = ({ list }) => {
+  return (
+    <>
+      {list.map((comment) => (
+        <div key={comment.id}>
+          <h3> {comment.name}</h3>
+          <p> {comment.body} </p>
+        </div>
+      ))}
+    </>
+  );
+};
+const AlbumList = ({ list }) => {
+  return (
+    <>
+      {list.map((album) => (
+        <div key={album.id}>
+          <h3> {album.title}</h3>
+        </div>
+      ))}
+    </>
+  );
+};
+const PhotoList = ({ list }) => {
+  return (
+    <>
+      {list.map((photo) => (
+        <div key={photo.id}>
+          <h3> {photo.title}</h3>
+          <img src={photo.thumbnailUrl} alt="photo" />
+        </div>
+      ))}
+    </>
+  );
+};
+const TodoList = ({ list }) => {
+  return (
+    <>
+      {list.map((todo) => (
+        <div key={todo.id}>
+          <h3> {todo.title} </h3>
+          <p> {todo.comleted.toString()} </p>
+        </div>
+      ))}
+    </>
+  );
+};
+const UserList = ({ list }) => {
+  return (
+    <>
+      {list.map((user) => (
+        <div key={user.id}>
+          <h3>
+            {user.name} -- {user.username} -- {user.email}
+          </h3>
+        </div>
+      ))}
+    </>
+  );
+};
+
+const urlBuilder = (resource) =>
+  `https://jsonplaceholder.typicode.com/${resource}`;
+
+const POSTS = "posts";
+const COMMENTS = "comments";
+const ALBUMS = "albums";
+const PHOTOS = "photos";
+const TODOS = "todos";
+const USERS = "users";
+
+const Components = {
+  [POSTS]: PostList,
+  [COMMENTS]: CommentList,
+  [ALBUMS]: AlbumList,
+  [PHOTOS]: PhotoList,
+  [TODOS]: TodoList,
+  [USERS]: UserList,
 };
 
 function App() {
   const tabs = [
-    { title: "posts", clickHandler: () => {} },
-    { title: "comments", clickHandler: () => {} },
-    { title: "albums", clickHandler: () => {} },
-    { title: "photos", clickHandler: () => {} },
-    { title: "todos", clickHandler: () => {} },
-    { title: "users", clickHandler: () => {} },
+    { title: POSTS, clickHandler: () => setSelectedTab(POSTS) },
+    { title: COMMENTS, clickHandler: () => setSelectedTab(COMMENTS) },
+    { title: ALBUMS, clickHandler: () => setSelectedTab(ALBUMS) },
+    { title: PHOTOS, clickHandler: () => setSelectedTab(PHOTOS) },
+    { title: TODOS, clickHandler: () => setSelectedTab(TODOS) },
+    { title: USERS, clickHandler: () => setSelectedTab(USERS) },
   ];
   const [selectedTab, setSelectedTab] = useState(tabs[0].title);
-  
+  const [data, setData] = useState({
+
+    [POSTS]: [],
+    [COMMENTS]: [],
+    [ALBUMS]: [],
+    [PHOTOS]: [],
+    [TODOS]: [],
+    [USERS]: [],
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    const response = await fetch(urlBuilder(selectedTab));
+    const json = await response.json();
+    setData({...data, [selectedTab]: json})
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [selectedTab]);
+
+  const ComponentToRender = Components[selectedTab];
   return (
     <div className="App">
       <Tabs tabs={tabs} selectedTab={selectedTab} />
+      {isLoading && !data[selectedTab].length ? <h1> LOADING... </h1> : (
+      <ComponentToRender list={data[selectedTab]} />
+      )}
     </div>
   );
 }
